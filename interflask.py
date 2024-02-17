c3edcac5ac47c50c1b627216dedff9c156237598
frappé <132247705+frappefactible@users.noreply.github.com>
2024-02-17 17:32:49 -0600

    Add files via upload

--- interflask.py
+++ interflask.py
@@ -4,7 +4,7 @@ from werkzeug.utils import secure_filename
 import os
 import tensorflow as tf
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 app = Flask(__name__)
 model = None
@@ -24,24 +24,52 @@ def upload_predict():
         image_file = request.files['image']
         if image_file:
             image = Image.open(image_file)
+            image_copy = image.copy()  # Hacer una copia de la imagen original
             image = image.resize((28, 28))  # or whatever size you need
+            image_array = np.array(image)
+            image_array = image_array.reshape(1, 28, 28, 3)  # or whatever your model expects
             image = np.array(image)
             image = image.reshape(1, 28, 28, 3)  # or whatever your model expects
-            predictions = model.predict(image)
+            #image_location = os.path.join(UPLOAD_FOLDER, image_file.filename)
+            #image_file.save(image_location)
+            #image = Image.open(image_location)
+            #image_copy = image.copy()  # Hacer una copia de la imagen
+            #image = image.resize((28, 28))  # or whatever size you need
+            #image_array = np.array(image)
+            #image_array = image_array.reshape(1, 28, 28, 3)  # or whatever your model expects
+            predictions = model.predict(image_array)
             print(predictions)
             predicted_class = np.argmax(predictions)
             confidence = predictions[0][predicted_class]
             confidence_percent = round(confidence * 100, 2)  # Redondear a 2 decimales
 
+            # Umbral de confianza
+            confidence_threshold = 69.99  # Ajusta este valor según tus necesidades
+
+            if confidence_percent < confidence_threshold:
+                return render_template('index.html', prediction="La imagen subida no parece estar relacionada con el cáncer de piel.", confidence="-")
+            
+            # Agregar texto a la copia de la imagen
+            draw = ImageDraw.Draw(image_copy)
+
+            # Calcula el tamaño del texto en función del tamaño de la imagen
+            width, height = image_copy.size
+            font_size = int(height / 20)  # Ajusta este valor según tus necesidades
+
+            font = ImageFont.truetype("arial.ttf", font_size)  # Asegúrate de tener la fuente arial.ttf en tu directorio de trabajo
+            text = f"Clase: {predicted_class}, Confianza: {confidence_percent}%"
+            draw.text((0, 0), text, (255, 255, 255), font=font)  # Cambia las coordenadas y el color según tus necesidades
+
+
             # Guardar la imagen subida en una carpeta temporal
             filename = secure_filename(image_file.filename)
             image_path = os.path.join('temp/', filename)
-            image_file.save(image_path)
+            image_copy.save(image_path)
 
             # Reabrir el archivo y guardarlo
-            image_file.seek(0)  # Retroceder al inicio del archivo
-            with open(image_path, 'wb') as f:
-                f.write(image_file.read())
+            #image_file.seek(0)  # Retroceder al inicio del archivo
+            #with open(image_path, 'wb') as f:
+            #    f.write(image_file.read())
 
             # Código para enviar el correo electrónico
             email = request.form['email']
@@ -49,8 +77,10 @@ def upload_predict():
             msg.body = f"La predicción tuvo un <span style='color:green;'>{confidence_percent}%</span> de precisión, detectando asi una clase <span style='color:red;'>{predicted_class}</span> dentro del modelo."
             msg.html = msg.body  # Set the HTML version of the body message
     
-            with app.open_resource(image_path) as fp:
-                msg.attach(filename, "image/jpeg", fp.read())
+            #with app.open_resource(image_path) as fp:
+            #    msg.attach(filename, "image/jpeg", fp.read())
+            with open(image_path, 'rb') as fp:
+                    msg.attach(filename, "image/jpeg", fp.read())
 
             mail.send(msg)
 
